5b9fcd56812c06fd70c4b9ac2d38ae3eaf9efe37
Neha B <nehabandlamudi@gmail.com>
2025-06-25 21:28:57 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'Homepage.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepOrange,
+        ),
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      home: Homepage(),
+    );
+  }
+}
+
---
+++ lib/service/http_service.dart
@@ -0,0 +1,42 @@
+import 'package:dio/dio.dart';
+import 'package:recip_app/consts.dart';
+
+class HTTPService{
+  static final HTTPService _singleton= HTTPService._internal();
+  final _dio=Dio();
+
+  factory HTTPService(){
+    return _singleton;
+  }
+  HTTPService._internal(){
+    setup();
+  }
+
+  Future<void> setup({String? bearerToken}) async{
+    final headers={
+      "Content-Type":"application/json",
+    };
+    final options=BaseOptions(
+      baseUrl: API_BASE_URL,
+      headers: headers,
+      validateStatus: (status){
+        if(status==null) return false;
+        return status<500;
+      },
+    );
+    _dio.options=options;
+  }
+
+  Future<Response?> get(String path)async{
+    try{
+      final response=await _dio.get(path);
+      return response;
+    }catch(e){
+      print(e);
+    }
+    return null;
+  }
+}
+
+
+
